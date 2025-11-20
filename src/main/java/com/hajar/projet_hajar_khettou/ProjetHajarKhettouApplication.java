3dcb599d8f52433b78ce0e5d6c6210ec4780cea2
hajar-khettou <simplehajar7504@gmail.com>
2025-11-20 10:50:37 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/hajar/projet_hajar_khettou/ProjetHajarKhettouApplication.java
@@ -0,0 +1,13 @@
+package com.hajar.projet_hajar_khettou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetHajarKhettouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetHajarKhettouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hajar/projet_hajar_khettou/ProjetHajarKhettouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hajar.projet_hajar_khettou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetHajarKhettouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
